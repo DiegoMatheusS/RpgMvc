bdb896f60b1aab1cdf6c026e473cc675210cb6f8
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2024-06-03 14:03:10 +0000

    feat: atualizado metodo post create

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -73,6 +73,39 @@ namespace RpgMvc.Controllers
                 return RedirectToAction("Create");
             }
         }
+        /*
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(PersonagemViewModel p)
+        {
+            try
+            {
+        HttpClient httpClient = new HttpClient();
+        string token = HttpContext.Session.GetString("SessionTokenUsuario");
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var content = new StringContent(JsonConvert.SerializeObject(p), 
+        content.Headers.ContentType = new MediaTypeHeaderValue("Application/json");
+        HttpResponseMessage response = await httpClient.PostAsync(uriBase, content);
+        string serialized = await response.Content.ReadAsStringAsync();
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            TempData["Mensagem"] = string.Format("Personagem {0}, Id {1} salvo com sucesso!", p.Nome, serialized);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            throw new System.Exception(serialized);
+        }
+    }
+    catch (System.Exception ex)
+    {
+        TempData["MensagemErro"] = ex.Message;
+        return RedirectToAction("Create");
+    }
+}
+
+        */
 
         [HttpGet]
         public ActionResult Create()
